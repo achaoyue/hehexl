5ae3b456ff3938d0db547f3d4b37bfeb8f1bc3e6
erniu <2509835001@qq.com>
2019-01-23 11:42:37 +0800

    头部OK“

---
+++ src/components/GameApp.ts
@@ -0,0 +1,17 @@
+class GameApp extends eui.Component implements  eui.UIComponent {
+	public constructor() {
+		super();
+	}
+
+	protected partAdded(partName:string,instance:any):void
+	{
+		super.partAdded(partName,instance);
+	}
+
+
+	protected childrenCreated():void
+	{
+		super.childrenCreated();
+	}
+	
+}
