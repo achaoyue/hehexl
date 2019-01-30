d623e0f8733d8aca55c6972c08339182bacfeb14
erniu <2509835001@qq.com>
2019-01-30 16:04:38 +0800

    190130

---
+++ src/SelfComponents.ts
@@ -0,0 +1,9 @@
+// TypeScript file
+
+window["HomeTabPanel"] = HomeTabPanel;
+window["RechangePanel"] = RechangePanel;
+window["WithdrawPanel"] = WithdrawPanel;
+window["GamePanel"] = GamePanel;
+window["VirtualJoystick"] = VirtualJoystick;
+window["HomeTabItem"] = HomeTabItem;
+window["HomeTabPanel"] = HomeTabPanel;
