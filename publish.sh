4c3df844e3d80def7c580c884558af15a62f4e33
mwy <2509835001@qq.com>
2019-02-16 23:38:39 +0800

    publish

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@
 if [ ! $1 ]; then
 echo "请指定目录"
 else
-rm -rf ./bin-release/web/$1/*
+rm -rf /Users/mouwenyao/egret/publish/hehexl/*
 cp -r ./bin-release/web/$1/* /Users/mouwenyao/egret/publish/hehexl
+cd /Users/mouwenyao/egret/publish/hehexl
+zip -r xxx.zip ./*
 fi
